111a628114671996bd2f7b3faa9ee4ae2a07d29b
PsiXoDeLiC <124059621+PsiXoDeLiC@users.noreply.github.com>
2024-04-10 18:28:13 +0400

    Update main.c

--- Shell/main.c
+++ Shell/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/wait.h>
@@ -10,58 +9,29 @@
 #define MAX_LENGTH 256
 extern char **environ;
 
-char command[MAX_LENGTH];// Хранит введенную команду
-char argument[5][MAX_LENGTH];// Хранит аргументы команды
+char *argument[MAX_LENGTH];// Хранит аргументы команды
 char directory[MAX_LENGTH];// Хранит путь текущей директории
-char comment[MAX_LENGTH];
-int carg ;
+int count;
 
-
-void code_processing(char input[]){
-    int i = 0;//Пропускаем прорбелы в начале строки
-    while (isspace(input[i]) && input[i] != '\0') {
-        i++;
-    }
-    int j = 0;// Записываем команду
-    while (input[i] != '\0' && !isspace(input[i])) {
-        command[j++] = input[i++];
-    }
-    command[j] = '\0';
-    //пропускаем пробелы перед аргументом
-    while (isspace(input[i]) && input[i] != '\0') {
-        i++;
-    }
-    carg = 0;
-    j = 0;//записываем аргумент
-    int n = 0;
-    while (input[i] != '\0') {
-        if(carg < 4){
-            if (isspace(input[i])){
-                argument[carg][j + 1] = '\0';
-                carg++;
-                i++;
-                j = 0;
-            }
-            argument[carg][j] = input[i];
-            j++;
-            i++;
-        }
-        else{
-            comment[n]  = input[i];
-            i++;
-            n++;
-        }
+void code_processing(char *input){ //разбиение строки на команду и аргументы
+    char *temp = strtok(input, " ");
+    count = 0;
+    while (temp != NULL){
+        argument[count++] = temp;
+        printf("%s ", argument[count-1]);
+        temp = strtok(NULL, " ");
 
     }
-    argument[carg][j] = '\0';
+    argument[count] = NULL;
 }
 
+
 void cd_processing(){
-    if (argument[0][0] == '\0') {//если нет аргумента узнать текущую директорию
+    if (argument[1] == NULL) {//если нет аргумента узнать текущую директорию
         printf("the current directory: %s\n", directory);
     }
     else {//если не найдена директория соответствующая аргументу сообщить об этом
-        if(chdir(argument[0]) != 0) {
+        if(chdir(argument[1]) != 0) {
             printf("The directory was not found\n");
         }
         else {//изменить дерикторию и переменную среды на указанный в аргументе
@@ -76,11 +46,11 @@ void dir_processing(){
     struct dirent *entry;
     char catalog[MAX_LENGTH];
     //при отсутствии аргумента показать содержимое текущей директории
-    if (argument[0][0] == '\0') {
+    if (argument[1] == NULL) {
         strcpy(catalog, directory);
     }
     else {
-        strcpy(catalog, argument[0]);
+        strcpy(catalog, argument[1]);
     }//если указанной дериктории не существует сообщить об этом
     dir = opendir(catalog);
     if (dir == NULL) {
@@ -104,10 +74,10 @@ void environ_processing(){//вывод переменных среды
 
 void echo_processing() {
     // Проход по каждому элементу массива argument и печать его
-    for (int i = 0; i < 5 && argument[i][0] != '\0'; ++i) {
+    for (int i = 1; i < count; ++i) {
         printf("%s ", argument[i]);
     }
-    printf("%s ", comment);
+    printf("\n");
 }
 
 void help_processing(){//вывод поддерживаемых комманд
@@ -122,34 +92,20 @@ void help_processing(){//вывод поддерживаемых комманд
     printf("quit: Exit the shell\n");
 }
 
-
 void child_processing() {
     pid_t pid = fork();
 
-    if (pid == 0) {
-        char* args[5]; // Массив для хранения команды и аргументов
-
-
-        // Заполнение остальных аргументов
-        for (int i = 0; i < carg && argument[i][0] != '\0'; ++i) {
-            args[i + 1] = argument[i];
-        }
-        args[carg + 1] = NULL; // Последний элемент должен быть NULL, как требует execvp
-        execvp(command, args);
-
-        // Если execvp вернется, значит, произошла ошибка
-        fprintf(stderr, "Command not found: %s\n", command);
+    if (pid == 0) {//если не удалось запустить дочерний процес сообщаем о неизвестной команде
+        execvp(argument[0], argument);
+        fprintf(stderr, "Command not found: %s\n", argument[0]);
         exit(EXIT_FAILURE);
-    } else if (pid < 0) {
+    } else if (pid < 0) {//если дочерний процесс выдал ошибку сообщаем о ней
         fprintf(stderr, "Fork failed\n");
-    } else {
+    } else {//иначе ждем заверщения дочернего процесса 
         int status;
-        waitpid(pid, &status, 0); // ожидание завершения дочернего процесса
+        waitpid(pid, &status, 0);
     }
 }
-
-
-
 
 
 int main(int argc, char *argv[]) {
@@ -171,6 +127,7 @@ int main(int argc, char *argv[]) {
 
         printf("myshell: %s> ", directory);//Приглашение командной строки
         fgets(input, sizeof(input), stdin);// Запрос ввода пользователя
+        input[strcspn(input, "\n")] = '\0';
 
         if (strcmp(input, "\n") == 0) {
             continue;//Пропускаем обработку пустой строки
@@ -181,28 +138,28 @@ int main(int argc, char *argv[]) {
 
 //Определение и исполнение комманды
 
-        if (strcmp(command, "quit") == 0) {//выход из оболочки
+        if (strcmp(argument[0], "quit") == 0) {//выход из оболочки
             break;
         }
-        else if (strcmp(command, "cd") == 0) {//Директория
+        else if (strcmp(argument[0], "cd") == 0) {//Директория
             cd_processing();
         }
-        else if (strcmp(command, "clr") == 0) {//очистить терминал
+        else if (strcmp(argument[0], "clr") == 0) {//очистить терминал
             system("clear");
         }
-        else if (strcmp(command, "dir") == 0) {//Содержимое каталога
+        else if (strcmp(argument[0], "dir") == 0) {//Содержимое каталога
             dir_processing();
         }//Показать содержимое среды
-        else if (strcmp(command, "environ") == 0) {
+        else if (strcmp(argument[0], "environ") == 0) {
             environ_processing();
         }// вывод на экран комментария указанного в аргументе
-        else if (strcmp(command, "echo") == 0) {
+        else if (strcmp(argument[0], "echo") == 0) {
             echo_processing();
         }//вывод руководства пользователя.
-        else if (strcmp(command, "help") == 0) {
+        else if (strcmp(argument[0], "help") == 0) {
             help_processing();
         }//приостановка операций оболочки до нажатия клавиши <Enter>.
-        else if (strcmp(command, "pause") == 0) {
+        else if (strcmp(argument[0], "pause") == 0) {
             printf("Press Enter to continue...");
             getchar();
         }
